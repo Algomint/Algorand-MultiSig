cc424377c27fe4e84b9cb36266f1a07d0406b98d
Owan Hunte <owanh@hotmail.com>
2022-07-29 09:11:57 -0400

    style(frontend): apply Prettier formatting rules

--- frontend/src/utils/algodClient.ts
+++ frontend/src/utils/algodClient.ts
@@ -1,14 +1,13 @@
-import algosdk from 'algosdk';
+import algosdk from "algosdk";
 
 async function initiateAlgodClient() {
-    const baseServer = 'https://testnet-algorand.api.purestake.io/ps2'
-    const port = '';
-    const token = { 'X-API-Key': 'ERJa19DtyE462CXdcoREb3mt9RQGEeM22i5CKXhz' };
+  const baseServer = "https://testnet-algorand.api.purestake.io/ps2";
+  const port = "";
+  const token = { "X-API-Key": "ERJa19DtyE462CXdcoREb3mt9RQGEeM22i5CKXhz" };
 
-    const algodClient = new algosdk.Algodv2(token, baseServer, port);
+  const algodClient = new algosdk.Algodv2(token, baseServer, port);
 
-    return algodClient
-
+  return algodClient;
 }
 
-export default initiateAlgodClient
+export default initiateAlgodClient;
