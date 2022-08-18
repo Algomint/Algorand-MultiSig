a52eb37fe5e893b7caf3d44b4d1ec491be0b35d2
nabetse00 <esteban.diezlaiz@gmail.com>
2022-08-18 16:39:59 +0200

    refector(algo-script): generateAccounts.js

--- algo-scripts/generateAccounts.js
+++ algo-scripts/generateAccounts.js
@@ -1,27 +1,34 @@
 const algosdk = require("algosdk");
-const fmt = {
-  bold: "\x1b[1m",
-  dim: "\x1b[2m",
-  reset: "\x1b[0m",
-};
+const {promises: fs} = require("fs");
+const filePath = "./addresses.txt";
 
-// generate accounts
-
-const numberOfAccounts = 5;
-
-for (let index = 1; index < numberOfAccounts+1; index++) {
-  const { sk: sk1, addr: addr1 } = algosdk.generateAccount();
-  const mnemonic1 = algosdk.secretKeyToMnemonic(sk1);
-  console.log(`
-${fmt.bold}${index}:${fmt.reset}
-${fmt.dim}Mnemonic:${fmt.reset} ${mnemonic1}
-${fmt.dim}Address:${fmt.reset} ${addr1}
-${fmt.dim}--------------------${fmt.reset}
-`);
+async function write(data){
+  await fs.appendFile(filePath, data, (err) => {
+    if (err) {
+      console.error(err);
+    }
+});
 }
 
-console.log(`
-${fmt.bold}TIP:${fmt.reset} You can send funds to your accounts using the testnet and betanet dispensers listed below:
+// generate accounts
+const numberOfAccounts = 5;
+(async () => {
+for (let index = 1; index < numberOfAccounts + 1; index++) {
+  const { sk: sk1, addr: addr1 } = algosdk.generateAccount();
+  const mnemonic1 = algosdk.secretKeyToMnemonic(sk1);
+  const data = `${index}:
+  Mnemonic: ${mnemonic1}
+  Address: ${addr1}
+  --------------------\n`;
+  console.log(data);
+  await write(data)
+}
+
+const end = `
+TIP: You can send funds to your accounts using the testnet and betanet dispensers listed below:
 * https://bank.testnet.algorand.network
 * https://bank.betanet.algodev.network/
-`);
+`
+console.log(end);
+await write(end);
+})();
